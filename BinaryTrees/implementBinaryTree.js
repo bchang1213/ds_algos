677de0e2f8b9016a7dc2dcc67695fbe54549a2db
bchang1213 <bchang952@gmail.com>
2022-05-27 01:16:30 +0900

    binary tree traversals

--- BinaryTrees/implementBinaryTree.js
+++ BinaryTrees/implementBinaryTree.js
@@ -57,13 +57,73 @@ const BinaryTree = function (root = null) {
 
     return findValue + " not in tree.";
   };
+
+  /**
+   * Depth First Traversal
+   *
+   * Pre order: root, left, right
+   * in-order: left, root, right - on a binary search tree, numbers will come out in ascending order.
+   * post-order: left, right, root
+   */
+
+  this.preOrder = function (root) {
+    console.log("PRE ORDER START");
+    if (root === null) {
+      return;
+    }
+
+    console.log(root.value);
+    this.preOrder(root.left);
+    this.preOrder(root.right);
+  };
+
+  this.inOrder = function (root) {
+    console.log("IN ORDER START");
+
+    if (root === null) {
+      return;
+    }
+
+    this.inOrder(root.left);
+    console.log(root.value);
+    this.inOrder(root.right);
+  };
+
+  this.postOrder = function (root) {
+    console.log("POST ORDER START");
+
+    if (root === null) {
+      return;
+    }
+
+    this.postOrder(root.left);
+    this.postOrder(root.right);
+    console.log(root.value);
+  };
+
+  /**
+   * Breadth First Traversal
+   * aka Level Order, visit everything at current level before advancing to next level.
+   */
 };
 
 let newTree = new BinaryTree();
 newTree.insert(25);
 newTree.insert(21);
+newTree.insert(22);
+newTree.insert(29);
 newTree.insert(26);
+newTree.insert(30);
 newTree.insert(10);
 newTree.insert(5);
 
-console.log(newTree.find(27));
+//Call Pre Order
+// console.log(newTree.preOrder(newTree.root));
+
+//Call In Order
+// console.log(newTree.inOrder(newTree.root));
+
+//Call Post Order
+console.log(newTree.postOrder(newTree.root));
+
+// console.log(JSON.stringify(newTree, null, 2));
